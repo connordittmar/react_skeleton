89fa3905ad87211556f19313369331688c337002
Connor Dittmar <connor.dittmar@gmail.com>
2018-07-16 09:50:37 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    //path: path.resolve('C:/users/m181524/projects/central_server/console/static/console') //this is for specific destination
+    //path: path.resolve('C:/users/blahblahblah/projects/central_server/console/static/console') //this is for writing to a specific destination
     path: path.resolve(__dirname, 'dist')
   },
   module: {
