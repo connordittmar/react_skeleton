6d6c9b8493a851ab458feb09a50a1edb3cc39017
connordittmar <connor.dittmar@gmail.com>
2018-08-01 10:45:33 -0400

    Remove batchfile

--- react_project_setup.bat
+++
@@ -1,8 +0,0 @@
-call npm init --yes
-call npm install --save react react-dom semantic-ui-react
-call npm install --save-dev webpack
-call npm install --save-dev babel-loader babel-core babel-cli
-call npm install --save-dev lodash babel-plugin-lodash
-call npm install --save-dev babel-preset-stage-1 babel-preset-react babel-preset-es2015
-call npm install --save-dev axios
-echo React Setup Complete
